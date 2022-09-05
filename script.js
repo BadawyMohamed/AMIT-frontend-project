c963dd2b40dd1e7333295fafda9b1c9a00140c3d
BadawyMohamed <muhamedm.badawy@gmail.com>
2022-09-05 16:50:00 +0200

    files created + navbar

---
+++ script.js
@@ -0,0 +1,25 @@
+var barButton = document.querySelector(".fa-bars");
+var sideNav = document.querySelector('.sideContainer')
+var navBar = document.querySelector('nav'); 
+var xButton = document.querySelector('.x');
+barButton.addEventListener("click", activeSideNav );
+xButton.addEventListener('click', closeSideNav);
+
+
+function show (element) {
+    element.style.display = "flex";
+}
+
+function hide (element) {
+    element.style.display = "none";
+}
+
+function activeSideNav (){
+    // hide(navBar);
+    show(sideNav);
+}
+
+function closeSideNav() {
+hide(sideNav);
+// show(navBar);
+}
